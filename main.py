776694dc469c097cb07337c0a35d0675514b0a3d
lincoln1155 <lincoln07oliveira@gmail.com>
2026-04-15 20:02:53 -0300

    Minor changes, now printing all info on last 20 matches Need to fix waiting time (since im using sincronous requests for now) TODO next is to refactor and use async requests

--- main.py
+++ main.py
@@ -11,25 +11,20 @@ def load_key():
 def get_summoner_info():
     print("Insert your region")
     print("[1] Americas | [2] Asia | [3] Europe ")
-    region = int(input())
     
-    if region == 1:
-        region = "americas"
-    elif region == 2:
-        region = "asia"
-    elif region == 3:
-        region = "europe"
+    region_map = {'1': 'americas', '2': 'asia', '3': 'europe'}
+    
+    choice = input('>> ')
+    region = region_map.get(choice)
 
-
-    print("Now insert your summmoner ID, as in player#tag")
-    summoner_id = input()
+    print("Now insert your summmoner ID (player#tag)")
+    summoner_id = input('>> ')
     summoner_id = summoner_id.split("#", 1)
 
     return summoner_id, region
 
 
 def request_puuid_by_summoner_id(summoner_id, region, key):
-    
     url = f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{summoner_id[0]}/{summoner_id[1]}?api_key={key}"
     puuid = requests.get(url).json()
     return puuid['puuid']
@@ -38,12 +33,18 @@ def request_puuid_by_summoner_id(summoner_id, region, key):
 def get_matchid_by_puuid(puuid, region, key):
     url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20&api_key={key}"
     match_ids = requests.get(url).json()
-    return match_ids[0]
+    return match_ids
 
 def get_match_data_by_id(match_id, region, key):
-    url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={key}"
-    match_data = requests.get(url).json()
-    return match_data
+    
+    data = []
+    for id in match_id:
+        url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/{id}?api_key={key}"
+        match_data = requests.get(url).json()
+        data.append(match_data)
+    print(data)
+    return data
+
 
 def process_match_data(match_data, puuid):
     participants = match_data['info']['participants']
@@ -60,10 +61,9 @@ def process_match_data(match_data, puuid):
             return stats
     return None
 
+key = load_key()
 
 info = get_summoner_info()
-
-key = load_key()
 
 puuid = request_puuid_by_summoner_id(info[0], info[1], key)
 
