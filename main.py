61465a17dc30e57d1dca865b17f243105bfee01b
lincoln1155 <lincoln07oliveira@gmail.com>
2026-04-16 22:06:29 -0300

    refactor code to use async requests! now working a LOT quicker

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 import requests
+import asyncio
+import aiohttp
 from dotenv import load_dotenv
+
+MAX_CONCURRENT_REQUESTS = 10
+semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
 
 def load_key():
     load_dotenv(".env")
@@ -23,28 +28,32 @@ def get_summoner_info():
 
     return summoner_id, region
 
-
-def request_puuid_by_summoner_id(summoner_id, region, key):
+async def request_puuid_by_summoner_id(session, summoner_id, region, key):
     url = f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{summoner_id[0]}/{summoner_id[1]}?api_key={key}"
-    puuid = requests.get(url).json()
-    return puuid['puuid']
+    
+    async with session.get(url) as response:
+        data = await response.json()
+        return data['puuid']
+    
     
 
-def get_matchid_by_puuid(puuid, region, key):
+async def get_matchid_by_puuid(session, puuid, region, key):
     url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20&api_key={key}"
-    match_ids = requests.get(url).json()
-    return match_ids
-
-def get_match_data_by_id(match_id, region, key):
     
-    data = []
-    for id in match_id:
-        url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/{id}?api_key={key}"
-        match_data = requests.get(url).json()
-        data.append(match_data)
-    print(data)
-    return data
+    async with session.get(url) as response:
+        return await response.json()
+    
 
+async def get_match_data_by_id(session, match_id, region, key):
+
+    async with semaphore:
+        url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={key}"
+        async with session.get(url) as response:
+            if response.status == '429':
+                print('Rate limit!')
+            
+            return await response.json()
+        
 
 def process_match_data(match_data, puuid):
     participants = match_data['info']['participants']
@@ -57,20 +66,32 @@ def process_match_data(match_data, puuid):
                 'kda': f"{player['kills']}/{player['deaths']}/{player['assists']}",
                 'gold': player['goldEarned'],
                 'cs': player['totalMinionsKilled'],
+                'result': player['win']
             }
             return stats
     return None
 
-key = load_key()
 
-info = get_summoner_info()
 
-puuid = request_puuid_by_summoner_id(info[0], info[1], key)
+async def main():
+    key = load_key()
+    summoner_id, region = get_summoner_info()
+    
+    async with aiohttp.ClientSession() as session:
+        print("Searching...")
 
-match_id = get_matchid_by_puuid(puuid, info[1], key)
+        puuid = await request_puuid_by_summoner_id(session, summoner_id, region, key)
 
-match_data = get_match_data_by_id(match_id, info[1], key)
+        match_ids = await get_matchid_by_puuid(session, puuid, region, key)
 
-player_stats = process_match_data(match_data, puuid)
+        tasks = [get_match_data_by_id(session, m_id, region, key) for m_id in match_ids]
 
-print(player_stats)
+        all_matches = await asyncio.gather(*tasks)
+    print(f"Last 20 matches: for {summoner_id[0]}#{summoner_id[1]}")
+    for match in all_matches:
+        stats = process_match_data(match, puuid)
+        if stats:
+            print(f"Role: {stats['role']}, Win: {stats['result']}, Champion: {stats['champion']}, KDA: {stats['kda']}, CS: {stats['cs']}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
