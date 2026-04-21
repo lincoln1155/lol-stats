6fd9a1bd155bf164467ba4f65d7bfad8da72f773
lincoln1155 <lincoln07oliveira@gmail.com>
2026-04-21 10:18:42 -0300

    Refactor code to use FastAPI web routes for now, the response is simply a json with info from last 20 matches route example: localhost/matches/asia/hide on bush-kr1

--- main.py
+++ main.py
@@ -1,61 +1,50 @@
 import os
-import requests
 import asyncio
 import aiohttp
-import fastapi
+from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
+from contextlib import asynccontextmanager
 
 MAX_CONCURRENT_REQUESTS = 10
 semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
 
-def load_key():
-    load_dotenv(".env")
-    SECRET_KEY = os.environ.get("RIOT_API_KEY")
-    return SECRET_KEY
+load_dotenv(".env")
+RIOT_KEY = os.environ.get("RIOT_API_KEY")
 
 
-def get_summoner_info():
-    print("Insert your region")
-    print("[1] Americas | [2] Asia | [3] Europe ")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.session = aiohttp.ClientSession()
     
-    region_map = {'1': 'americas', '2': 'asia', '3': 'europe'}
-    
-    choice = input('>> ')
-    region = region_map.get(choice)
+    yield
+    await app.state.session.close()
 
-    print("Now insert your summmoner ID (player#tag)")
-    summoner_id = input('>> ')
-    summoner_id = summoner_id.split("#", 1)
+app = FastAPI(lifespan=lifespan)
 
-    return summoner_id, region
-
-async def request_puuid_by_summoner_id(session, summoner_id, region, key):
-    url = f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{summoner_id[0]}/{summoner_id[1]}?api_key={key}"
+async def request_puuid_by_summoner_id(session, riot_id, region, key):
+    riot_id = riot_id.split("-")
+    url = f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{riot_id[0]}/{riot_id[1]}?api_key={key}"
     
     async with session.get(url) as response:
         data = await response.json()
         return data['puuid']
     
-    
-
 async def get_matchid_by_puuid(session, puuid, region, key):
     url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20&api_key={key}"
     
     async with session.get(url) as response:
         return await response.json()
     
-
 async def get_match_data_by_id(session, match_id, region, key):
 
     async with semaphore:
         url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={key}"
         async with session.get(url) as response:
-            if response.status == '429':
+            if response.status == 429:
                 print('Rate limit!')
             
             return await response.json()
         
-
 def process_match_data(match_data, puuid):
     participants = match_data['info']['participants']
     for player in participants:
@@ -67,32 +56,33 @@ def process_match_data(match_data, puuid):
                 'kda': f"{player['kills']}/{player['deaths']}/{player['assists']}",
                 'gold': player['goldEarned'],
                 'cs': player['totalMinionsKilled'],
-                'result': player['win']
+                'win': player['win']
             }
             return stats
     return None
 
+@app.get("/matches/{region}/{riot_id}")
+async def search_matches(region: str, riot_id: str):
+    session = app.state.session
 
+    puuid = await request_puuid_by_summoner_id(session, riot_id, region, RIOT_KEY)
+    if not puuid:
+        raise HTTPException(status_code=404, detail="Player not found")
+    match_ids = await get_matchid_by_puuid(session, puuid, region, RIOT_KEY)
 
-async def main():
-    key = load_key()
-    summoner_id, region = get_summoner_info()
-    
-    async with aiohttp.ClientSession() as session:
-        print("Searching...")
+    tasks = [get_match_data_by_id(session, m_id, region, RIOT_KEY) for m_id in match_ids]
 
-        puuid = await request_puuid_by_summoner_id(session, summoner_id, region, key)
+    all_matches = await asyncio.gather(*tasks, return_exceptions=True)
 
-        match_ids = await get_matchid_by_puuid(session, puuid, region, key)
+    print(f"Last 20 matches: for {riot_id[0]}#{riot_id[1]}")
 
-        tasks = [get_match_data_by_id(session, m_id, region, key) for m_id in match_ids]
-
-        all_matches = await asyncio.gather(*tasks)
-    print(f"Last 20 matches: for {summoner_id[0]}#{summoner_id[1]}")
+    results = []
     for match in all_matches:
         stats = process_match_data(match, puuid)
         if stats:
-            print(f"Role: {stats['role']}, Win: {stats['result']}, Champion: {stats['champion']}, KDA: {stats['kda']}, CS: {stats['cs']}")
-
-if __name__ == "__main__":
-    asyncio.run(main())
+            results.append(stats)
+    
+    return {
+        "summoner": f"{riot_id}",
+        "matches": results
+    }
